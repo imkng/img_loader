81cd5f498d6ed9c39686aad7b202987ce692ff2c
krnand <1india.kng@gmail.com>
2024-04-14 17:40:35 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import ImageUploader from './components/ImageUploader'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <ImageUploader/>
+    </>
+  )
+}
+
+export default App
